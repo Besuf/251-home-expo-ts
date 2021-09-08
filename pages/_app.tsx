03218e22e752c26a6243bd084ab990eabf1a2b38
Besuf <besufkeat@gmail.com>
2021-09-08 14:02:32 +0300

    added progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import "../styles/base.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import NextNprogress from "nextjs-progressbar";
 
 import type { AppProps } from "next/app";
+import { Fragment } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <NextNprogress
+        color="#FEE715"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={4}
+        showOnShallow={true}
+      />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 export default MyApp;
