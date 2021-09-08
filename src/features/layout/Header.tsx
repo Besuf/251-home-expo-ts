f218d6dec573d28406b67aaf97b12fbab3b43bf6
Besuf <besufkeat@gmail.com>
2021-09-09 01:12:21 +0300

    Responsive modal animation for drawer

--- src/features/layout/Header.tsx
+++ src/features/layout/Header.tsx
@@ -10,6 +10,7 @@ import DarkFacebook from "components/icons/DarkFacebook";
 import DarkTwitter from "components/icons/DarkTwitter";
 import DarkInstagram from "components/icons/DarkInstagram";
 import ScrollDisable from "components/shared/ScrollDisable";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Header = () => {
   const [openMenu, setOpenMenu] = useState(false);
@@ -42,6 +43,9 @@ const Header = () => {
   const toggleMenu = () => {
     setOpenMenu(!openMenu);
   };
+  let mediaMatch = false;
+  if (typeof window !== undefined)
+    mediaMatch = window.matchMedia("(min-width: 1024px)").matches;
 
   return (
     <div
@@ -91,62 +95,79 @@ const Header = () => {
         </div>
       </div>
 
-      {openMenu && (
-        <div className="fixed inset-0 bg-textPrimary bg-opacity-50">
-          <ScrollDisable />
-        </div>
-      )}
-      {openMenu && (
-        <div className="fixed inset-y-0 bg-primary bg-opacity-90  w-full md:w-[400px] md:right-0  lg:left-0  text-textPrimary p-8">
-          <div className="flex justify-end">
-            <span className="cursor-pointer">
-              <DarkCloseIcon onClick={toggleMenu} className="h-8 w-8" />
-            </span>
-          </div>
-          <ul className="flex flex-col gap-2 text-xl">
-            <li>
-              <NextLink href="/">
-                <a className="pb-1 border-b border-transparent hover:border-textPrimary transition duration-200 ease-in">
-                  Home
-                </a>
-              </NextLink>
-            </li>
-            <li>
-              <NextLink href="/gallery">
-                <a className="pb-1 border-b border-transparent hover:border-textPrimary transition duration-200 ease-in">
-                  Gallery
-                </a>
-              </NextLink>
-            </li>
-            <li>
-              <NextLink href="/visitor-subscription">
-                <a className="pb-1 border-b border-transparent hover:border-textPrimary transition duration-200 ease-in">
-                  User Registration
-                </a>
-              </NextLink>
-            </li>
-            <li>
-              <NextLink href="/exhibitor-subscription">
-                <a className="pb-1 border-b border-transparent hover:border-textPrimary transition duration-200 ease-in">
-                  Exhibitor Registration
-                </a>
-              </NextLink>
-            </li>
-          </ul>
+      <AnimatePresence>
+        {openMenu && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+            className="fixed inset-0 bg-textPrimary bg-opacity-50"
+          >
+            <ScrollDisable />
+          </motion.div>
+        )}
+      </AnimatePresence>
 
-          <div className="flex mt-10 gap-6">
-            <span className="cursor-pointer">
-              <DarkFacebook />
-            </span>
-            <span className="cursor-pointer">
-              <DarkTwitter />
-            </span>
-            <span className="cursor-pointer">
-              <DarkInstagram />
-            </span>
-          </div>
-        </div>
-      )}
+      <AnimatePresence>
+        {openMenu && (
+          <motion.div
+            initial={{ x: mediaMatch ? -1000 : 1000 }}
+            animate={{ x: 0 }}
+            exit={{ x: mediaMatch ? -1000 : 1000 }}
+            transition={{ duration: 0.5 }}
+            className="fixed inset-y-0 bg-primary bg-opacity-90  w-full md:w-[400px] md:right-0  lg:left-0  text-textPrimary p-8"
+          >
+            <div className="flex justify-end">
+              <span className="cursor-pointer">
+                <DarkCloseIcon onClick={toggleMenu} className="h-10 w-10" />
+              </span>
+            </div>
+            <ul className="flex flex-col gap-4 text-2xl">
+              <li>
+                <NextLink href="/">
+                  <a className="pb-1 border-b border-transparent hover:border-textPrimary transition duration-200 ease-in">
+                    Home
+                  </a>
+                </NextLink>
+              </li>
+              <li>
+                <NextLink href="/gallery">
+                  <a className="pb-1 border-b border-transparent hover:border-textPrimary transition duration-200 ease-in">
+                    Gallery
+                  </a>
+                </NextLink>
+              </li>
+              <li>
+                <NextLink href="/visitor-subscription">
+                  <a className="pb-1 border-b border-transparent hover:border-textPrimary transition duration-200 ease-in">
+                    User Registration
+                  </a>
+                </NextLink>
+              </li>
+              <li>
+                <NextLink href="/exhibitor-subscription">
+                  <a className="pb-1 border-b border-transparent hover:border-textPrimary transition duration-200 ease-in">
+                    Exhibitor Registration
+                  </a>
+                </NextLink>
+              </li>
+            </ul>
+
+            <div className="flex mt-10 gap-7">
+              <span className="cursor-pointer">
+                <DarkFacebook className="h-8 w-8" />
+              </span>
+              <span className="cursor-pointer">
+                <DarkTwitter className="h-8 w-8" />
+              </span>
+              <span className="cursor-pointer">
+                <DarkInstagram className="h-8 w-8" />
+              </span>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
